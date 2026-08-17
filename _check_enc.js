c6bd6ea9b2035a115944cf8f9d82fc6f7a67a3e7
Davi <andrielysom@gmail.com>
2026-08-16 21:37:48 -0300

    vai

--- _check_enc.js
+++
@@ -1,7 +0,0 @@
-var fs = require('fs');
-var b = fs.readFileSync('public/cadastro.html');
-console.log('bytes', b.length);
-console.log('BOM?', b[0] === 0xEF && b[1] === 0xBB && b[2] === 0xBF);
-var s = b.toString('utf8');
-console.log('tem acento Area?', s.includes('Área'));
-console.log('tem ?-subst', s.includes('\uFFFD'));
